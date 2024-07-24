b4f4db1deed6cfbe88fe976e64a106053c0a3f6f
Citrin <lima.stephanielemos@gmail.com>
2024-07-24 15:01:15 -0300

    Fist commit

---
+++ randomPhrases/src/test/java/br/com/citrinstar/randomPhrases/RandomPhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.citrinstar.randomPhrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
